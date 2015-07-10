51766a7f23c7cc7ffbff9b42ebc921341d72d63b
srasel <shahnewaz_113@yahoo.com>
2015-07-10 18:22:35 +0000

    1. 'age_group' replaced with 'Age Group' 2. CTE is replaced with while loop to list available period

--- StatsQuery.sql
+++ StatsQuery.sql
@@ -34,10 +34,14 @@ begin
 	select x.col.value('.', 'varchar(100)') AS [text()]
 	FROM @XmlStr.nodes('//root//query//SELECT') x(col)
 
+	--select * from #select 
+
 	select s.* into #A 
 	from #select s left join vDimDetails d
 	on s.name = d.[-t-id]
 	where d.[-t-id] is null
+
+	--select * from #A
 
 	if(@@ROWCOUNT = 0 or (select count(*) from #A)=0)
 	begin
@@ -80,10 +84,7 @@ begin
 			insert into #wheregeo
 			select id [Country Code] from DimGeo --where cat = (select top 1 * from #wherecat)
 		end
-
-		
 	end
-	
 	
 	insert into #wheretime
 	select iif(isnull(PARSENAME([val],2),PARSENAME([val],1))='*',-1,isnull(PARSENAME([val],2),PARSENAME([val],1)))
@@ -102,30 +103,58 @@ begin
 		select min(period),max(period) from DimTime
 	end
 
-	;with cte(period) as
-	(
-		select minTime from #wheretime
-		union all
-		select c.period + 1
-		from cte c
-		where c.period <= (select maxTime from #wheretime)
-	)
-	select * into #time
-	from cte;
+	--select * from #wheretime
+
+	declare @start int
+	declare @end int
+	declare @counter int
+
+	create table #time (period int)
+
+	select @start = minTime, @end = maxTime from #wheretime
+	set @counter = @start
+
+	while @counter <= @end 
+	begin
+		insert into #time
+		select @counter
+		set @counter = @counter + 1;
+	end
+	--;with cte(period) as
+	--(
+	--	select minTime from #wheretime
+	--	union all
+	--	select c.period + 1
+	--	from cte c
+	--	where c.period < (select maxTime from #wheretime)
+	--)
+	--select * into #time
+	--from cte;
+
+	--select * from #time order by period
 	
 	insert into #whereind
 	select s.name
 	from #select s left join vDimDetails d
 	on s.name = d.[-t-id]
 	where d.[-t-id] is null
-	/*select x.col.value('.', 'varchar(100)') AS [text()]
-	FROM @XmlStr.nodes('//root//query//WHERE//quantity') x(col)
-	
-	--select * from #select
-	select * from #wheregeo
-	select * from #wheretime
-	select * from #whereind
-	*/
+
+	declare @measure varchar(20)
+	select @measure = name from #select where name like 'age%'
+
+	declare @kount int
+	select @kount = count(*) from #select where name like 'age%'or name like 'pop%'
+
+	if(@kount > 1)
+	begin
+		truncate table #whereind
+		insert into #whereind
+		select [Indicator Code] from DimIndicators
+		where [Indicator Code] like case when len(@measure) > 3 then @measure + '%' else 'age_group%' end
+	end
+
+	--select * from #whereind
+
 	
 	insert into #from
 	select x.col.value('.', 'varchar(100)') AS [text()]
@@ -137,13 +166,21 @@ begin
 	
 	declare @colInFinalSelect nvarchar(max)
 	select @colInFinalSelect = STUFF((
-	select (',' + 'isnull([' + s.name + '],2013) ['  + s.name +']'  ) as [text()]
+	select (',' + '([' + s.name + ']) ['  + s.name +']'  ) as [text()]
 	from #select s left join vDimDetails dd
 	on dd.[-t-id] = s.name
 	where dd.cName is not null
 	for xml path ('')),1,1,'')
 	--select @dCols
 
+	--select * from #select
+	if((select count(*) from #select where name like 'age_group%')>=1)
+	begin
+		select @colInFinalSelect = coalesce(@colInFinalSelect+', '''+  replace(s.name,'age_group_','Age Group ') +''' [' + s.name + ']',@colInFinalSelect)
+		from #select s where s.name like 'age_group%' 
+	end
+
+	--select @colInFinalSelect
 
 	declare @colInQuerySelection nvarchar(max)
 	select @colInQuerySelection = STUFF((
@@ -168,10 +205,19 @@ begin
 
 	declare @indColInSelect nvarchar(max)
 	select @indColInSelect = STUFF((
-	select (',' + 'isnull([' + s.name + '],0) [' + s.name + ']') as [text()]
+	select (',' + '([' + s.name + ']) [' + s.name + ']') as [text()]
 	from #whereind s
 	for xml path ('')),1,1,'')
 	
+	declare @interimSelect varchar(100)
+	set @interimSelect = '[Indicator Code]'
+
+	if(@kount > 1)
+	begin
+		set @indCol = '[pop]'
+		set @indColInSelect = '[pop]'
+		set @interimSelect = '''pop'''
+	end
 	
 
 	--if(@@ROWCOUNT = 0 or (select top 1 name from #wherecat)='')
@@ -209,36 +255,46 @@ begin
 	left join (select * from cte where rnk = (select max(rnk) from cte)) c 
 	on dc.[Short Name] = c.name where c.name is not null 
 	
+	--select * from #time
+
+	DECLARE @parmDefinition nvarchar(500);
+	set @parmDefinition = N'@start int, @end int'
+
 	set @dyn_sql = N'
 	select ' + @colInFinalSelect + ','  + @indColInSelect + ' 
 	from (
-		select * from (select * from (select ' + @colInQuerySelection + ', sum(f.Value) val, di.[Indicator Code]
+		select ' + @colInFinalSelect + ', val, ' + @interimSelect + ' [Indicator Code]
+		from (select ' + @colInQuerySelection + ', sum(f.Value) val,  di.[Indicator Code]
 		from dbo.FactFinal f 
 		left join (select * from #geoFinal) dc
 		on f.[Country Code] = dc.ID
-		left join ( select i.* from DimIndicators i left join #whereind w on i.[Indicator Code] = w.name where w.name is not null) di
+		left join ( select i.[ID],i.[dataSourceID], [Indicator Code], i.[Indicator Name] from DimIndicators i left join #whereind w on i.[Indicator Code] = w.name where w.name is not null) di
 		on f.[Indicator Code] = di.ID
-		,#time t --#wheretime t 
+		, #time t --#wheretime t 
+		--on t.period = f.Period
 		where dc.ID is not null
 		and f.DataSourceID = (select top 1 ID from DimDataSource inner join #from on DataSource = tab)
 		and di.ID is not null
 		and f.Period = t.period--(Period >=t.minTime and Period <= t.maxTime)
-		group by ' + @colInGroupBy + ', di.[Indicator Code]) b where val > 0)c
-	)A pivot
+		--and f.Period between @start and @end
+		group by ' + @colInGroupBy + ', di.[Indicator Code]) b --where val > 0
+	)A 
+	pivot
 	(
 		sum(val)
 		for [Indicator Code] in (' + @indCol + ')
 	) as pvt
 	'
-	--print @dyn_sql
-	execute sp_executesql @dyn_sql
+	print @dyn_sql
+	execute sp_executesql @dyn_sql, @parmDefinition, @start = @start, @end = @end
 	
 	/*
 execute StatsQuery 
 '
 <root><query><SELECT>geo</SELECT><SELECT>geo.cat</SELECT><SELECT>geo.name</SELECT><SELECT>time</SELECT>
-<SELECT>gdp_per_capita</SELECT><WHERE><geo>eur</geo><geo.cat>region</geo.cat>
-<time>2000-2001</time><quantity /></WHERE><FROM>spreedsheet</FROM></query><lang>en</lang></root>
+<SELECT>age</SELECT>
+<SELECT>pop</SELECT><WHERE><geo>eur</geo><geo.cat>region</geo.cat>
+<time>2050-2100</time><quantity>age_group_0-14</quantity></WHERE><FROM>spreedsheet</FROM></query><lang>en</lang></root>
 '
 */
 
@@ -247,4 +303,10 @@ end
 
 GO
 
+execute StatsQuery 
+'
+<root><query><SELECT>geo</SELECT><SELECT>time</SELECT><SELECT>lex</SELECT><WHERE><geo>abw</geo>
+<geo.cat>country</geo.cat><time>1990-2100</time><quantity /></WHERE>
+<FROM>spreedsheet</FROM></query><lang>en</lang></root>
+'
 
